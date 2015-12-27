7b2307cbe84fe3b3e4da03b05f0faeda03b944cb
CapybaraCreations <capybaracreations@gmail.com>
2015-12-27 01:34:49 +0100

    ZAD_2

--- Android/Patryk.Krawczyk/app/src/main/java/com/blstreampatronage/patrykkrawczyk/MainActivity.java
+++ Android/Patryk.Krawczyk/app/src/main/java/com/blstreampatronage/patrykkrawczyk/MainActivity.java
@@ -1,20 +1,21 @@
 package com.blstreampatronage.patrykkrawczyk;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.EditText;
-import android.widget.ImageView;
+import android.webkit.WebView;
 import android.widget.TextView;
-import android.util.Patterns;
-import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView userEmailTextView;
+    private TextView userPasswordTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +33,29 @@ public class MainActivity extends AppCompatActivity {
             View decorView = getWindow().getDecorView();
             decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
         }
+
+        SharedPreferences sharedPref = getSharedPreferences("UserInformation", this.MODE_PRIVATE);
+        String userEmail    = sharedPref.getString(getString(R.string.userEmailKey), null);
+        String userPassword = sharedPref.getString(getString(R.string.userPasswordKey), null);
+
+        userEmailTextView    = (TextView) findViewById(R.id.mainUserEmail);
+        userPasswordTextView = (TextView) findViewById(R.id.mainUserPassword);
+
+        userEmailTextView.setText(userEmail);
+        userPasswordTextView.setText(userPassword);
+    }
+
+    public void onLogOutUser(View v) {
+        SharedPreferences sharedPref    = getSharedPreferences("UserInformation", this.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        editor.putString(getString(R.string.userEmailKey),    null);
+        editor.putString(getString(R.string.userPasswordKey), null);
+        editor.commit();
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 
 }
